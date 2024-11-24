302783fb8bdc8b97e7640e30c6b8fe66e06790ed
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-11-24 19:17:53 +0300

    first commit

---
+++ app/src/main/java/com/littlelemon/menu/ProductActivity.kt
@@ -0,0 +1,26 @@
+package com.littlelemon.menu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ProductActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val title = intent.getStringExtra(MainActivity.KEY_TITLE) ?: ""
+        val price = intent.getDoubleExtra(MainActivity.KEY_PRICE, 0.0)
+        val image = intent.getIntExtra(MainActivity.KEY_IMAGE, -1)
+        val category = intent.getStringExtra(MainActivity.KEY_CATEGORY) ?: ""
+        val productItem = ProductItem(title, price, category, image)
+
+        setContent { ProductDetails(productItem) }
+    }
+
+    companion object {
+        const val KEY_TITLE = "title"
+        const val KEY_PRICE = "price"
+        const val KEY_IMAGE = "image"
+        const val KEY_CATEGORY = "category"
+    }
+}
